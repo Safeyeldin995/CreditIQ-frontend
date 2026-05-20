0587df698816adc7968e4fb27a0b87aa6d07424d
Safeyeldin995 <safeyeldin2@yahoo.com>
2026-05-20 17:10:06 +0300

    Update NewApplication.jsx

--- src/pages/NewApplication.jsx
+++ src/pages/NewApplication.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { supabase, generateReferenceCode } from '../lib/supabase'
+import { supabase, generateReferenceCode } from '../supabase'
 import Layout from '../components/Layout'
 import { useToast } from '../components/Toast'
 
